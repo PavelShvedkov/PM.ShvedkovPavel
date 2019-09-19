df11d60ff4352e69362cf3e6c9d8059f67090f1e
PavelShvedkov <55479979+PavelShvedkov@users.noreply.github.com>
2019-09-19 19:20:18 +0300

    Add files via upload

---
+++ PM.Shvedkov.18.09.2019/Task11(д)/Task11(d).cpp
@@ -0,0 +1,26 @@
+// Лабораторная работа 3. Задание 11 д)
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	float x, y;
+	cout << " Enter coordinates\n X:= ";
+	cin >> x;
+	cout << " Y:= ";
+	cin >> y;
+	if (x >= 0 and x * x + y * y <= 1)
+	{
+		cout << "Point inside the figure" << endl;
+	}
+	else
+	{
+		if (-1 <= x <= 0 and x * x + y * y <= abs(x) + abs(y))
+			cout << "Point inside the figure" << endl;
+
+		else
+			cout << "Point outside the figure" << endl;
+	}
+	return 0;
+}
