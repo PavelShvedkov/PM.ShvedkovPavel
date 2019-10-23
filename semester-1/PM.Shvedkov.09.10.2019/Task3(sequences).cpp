63e3c27648c99589e99e3192f49b5fde6271d97b
PavelShvedkov <55479979+PavelShvedkov@users.noreply.github.com>
2019-10-23 11:48:22 +0300

    Update Task3(sequences).cpp

--- semester-1/PM.Shvedkov.09.10.2019/Task3(sequences).cpp
+++ semester-1/PM.Shvedkov.09.10.2019/Task3(sequences).cpp
@@ -1,7 +1,5 @@
 void sequence(int number, short& requality, short& rmore, short& rsmaller, short& rmorePerUnit, short& rsmallerPerUnit)
 {
-	int count = 0;
-
 	number = abs(number);
 
 	for (; number >= 10; number /= 10)
