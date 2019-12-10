e26f1e99d8cd3bbd8cee00ff8d6d280b132ab58b
Fifty_Fifty <pashka547@yande.by>
2019-12-10 17:21:03 +0300

    Home Work 04.12

---
+++ semester-1/PM.Shvedkov.04.12.2019/Sort by lenght.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Service.h"
+#include "WorkWithArrays.h"
+#include"WorkWithNumbers.h"
+#include "WorkWithStrings.h"
+
+using namespace std;
+
+int main()
+{
+	char source[] = "Learn how to use Visual..,.,.,.,/. Studio to@@@@ develop,,,,,, // /.,., applications,"
+		"services, and tools? in the          language of your choice, for"
+		"your, platforms and. devices";
+
+	int k = 0;
+
+	char** words = createList(source, k);
+	
+	lenghtSortList(words, k);
+	displayList(words, k);
+	
+	return 0;
+}
---
+++ semester-1/PM.Shvedkov.04.12.2019/Sort by symbol enterence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Service.h"
+#include "WorkWithArrays.h"
+#include"WorkWithNumbers.h"
+#include "WorkWithStrings.h"
+
+using namespace std;
+
+int main()
+{
+	char source[] = "Learn how to use Visual..,.,.,.,/. Studio to@@@@ develop,,,,,, // /.,., applications,"
+		"services, and tools? in the          language of your choice, for"
+		"your, platforms and. devices";
+
+	int k = 0;
+	char** words = createList(source, k);
+	char symbol = 'z';
+	//cin >> symbol;
+	
+	symbolEnterenceSortList(words, k, symbol);
+	displayList(words, k);
+	
+	return 0;
+}
