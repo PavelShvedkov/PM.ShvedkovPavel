0db6f0999598d8b3cd7425be920ba9f39d01dac1
Fifty_Fifty <pashka547@yande.by>
2020-04-07 22:14:08 +0300

    Update 1.3

--- semester-2/HomeWork11.03/Array.cpp
+++ semester-2/HomeWork11.03/Array.cpp
@@ -1,4 +1,5 @@
 #include "Array.h"
+#include <ctime>
 
 using namespace std;
 
@@ -110,6 +111,122 @@ void Array::sort(Comparer comparer)
 		i++;
 	}
 }
+void Array::mergeSort(Array& key, Comparer comparator)
+{
+	int length = this->getLength();
+
+	if (length != key.getLength())
+	{
+		throw std::invalid_argument(" Keys do not match array!");
+	}
+
+	int mid = length / 2, h = 1;
+
+	if (length % 2 == 1)
+	{
+		mid++;
+	}
+
+	Array buffer = *this;
+	Array sourcesBuffer(length);
+	Array keysBuffer(length);
+	int step;
+
+	while (h < length)
+	{
+		step = h;
+		int i = 0;
+		int j = mid;
+		int k = 0;
+		while (step <= mid)
+		{
+			while ((i < step) && (j < length) && (j < (mid + step)))
+			{
+				if (comparator(key[i], key[j]) > 0)
+				{
+					keysBuffer[k] = key[i];
+					sourcesBuffer[k] = buffer[i];
+					i++;
+					k++;
+				}
+				else
+				{
+					keysBuffer[k] = key[j];
+					sourcesBuffer[k] = buffer[j];
+					j++;
+					k++;
+				}
+			}
+			while (i < step)
+			{
+				keysBuffer[k] = key[i];
+				sourcesBuffer[k] = buffer[i];
+				i++;
+				k++;
+			}
+			while ((j < (mid + step)) && (j < length))
+			{
+				keysBuffer[k] = key[j];
+				sourcesBuffer[k] = buffer[j];
+				j++;
+				k++;
+			}
+
+			step = step + h;
+		}
+
+		h = h * 2;
+
+		for (i = 0; i < length; i++)
+		{
+			key[i] = keysBuffer[i];
+			buffer[i] = sourcesBuffer[i];
+		}
+	}
+
+	*this = buffer;
+}
+void Array::quickSort(Array& key, int first, int last, Comparer comparator)
+{
+	if (this->getLength() != key.getLength())
+	{
+		throw std::invalid_argument(" Keys do not match array!");
+	}
+
+	Array buffer = *this;
+	int f = first, l = last, mid = key[(f + l) / 2];
+
+	do
+	{
+		while (comparator(key[f], mid) > 0)
+		{
+			++f;
+		}
+		while (comparator(mid, key[l]) > 0)
+		{
+			--l;
+		}
+
+		if (f <= l)
+		{
+			swap(key[f], key[l]);
+			swap(buffer[f], buffer[l]);
+			++f;
+			--l;
+		}
+	} while (f < l);
+
+	*this = buffer;
+
+	if (first < l)
+	{
+		this->quickSort(key, first, l, comparator);
+	}
+	if (f < last)
+	{
+		this->quickSort(key, f, last, comparator);
+	}
+}
 void Array::inverted()
 {
 	int n = getLength();
@@ -121,9 +238,11 @@ void Array::inverted()
 }
 void Array::randomFilling(int limit)
 {
+	srand(time(NULL));
+
 	for (int i = 0; i < getLength(); i++)
 	{
-		setItem(i, rand() % limit / 2);
+		setItem(i, (rand() % limit / 2) * pow(-1, rand() % 2));
 	}
 }
 void Array::shuffle()
@@ -148,7 +267,7 @@ void Array::shift(int steps, bool direction)
 		{
 			int temp = this->array[length - 1];
 
-			for (int j = length; j >0; j--)
+			for (int j = length; j > 0; j--)
 			{
 				this->array[j] = this->array[j - 1];
 			}
@@ -162,7 +281,7 @@ void Array::shift(int steps, bool direction)
 		{
 			int temp = this->array[0];
 
-			for (int j = 0; j <length; j++)
+			for (int j = 0; j < length; j++)
 			{
 				this->array[j] = this->array[j + 1];
 			}
@@ -171,6 +290,98 @@ void Array::shift(int steps, bool direction)
 		}
 	}
 }
+void Array::writeBinFile(const char* path)
+{
+	ofstream out(path, ios::binary);
+	int length = this->getLength();
+	Array buffer=*this;
+
+	if (!out.is_open())
+	{
+		cout << "Cannot open file to read!" << endl;
+		system("pause");
+		exit(1);
+	}
+
+	out.write((char*)&length, sizeof(int));
+
+	for (int i = 0; i < length; ++i)
+	{
+		out.write((char*)&buffer[i], sizeof(int));
+	}
+
+	out.close();
+}
+void Array::readBinFile(const char* path)
+{
+	ifstream in(path, ios::binary);
+	int lengthDest = 0;
+
+	if (!in.is_open())
+	{
+		cout << "Cannot open file to read!" << endl;
+		system("pause");
+		exit(1);
+	}
+
+	in.read((char*)&lengthDest, sizeof(int));
+
+	Array buffer(lengthDest);
+
+	for (int i = 0; i < lengthDest; ++i)
+	{
+		in.read((char*)&buffer[i], sizeof(buffer[i]));
+	}
+
+	in.close();
+
+	*this = buffer;
+}
+void Array::deleteRepits()
+{
+	for (int i = 0, k = 0; i < this->getLength(); ++i)
+	{
+		for (int j = i + 1; j < this->getLength(); ++j)
+		{
+			if (this->getItem(i) == this->getItem(j))
+			{
+				this->deleteElement(j);
+			}
+		}
+	}
+}
+void Array::deleteElement(int index)
+{
+	int length = this->getLength();
+
+	if (index >= length)
+	{
+		throw std::invalid_argument(" Index can't be more than source length !");
+	}
+
+	if (index < 0)
+	{
+		throw std::invalid_argument(" Index can't be less than zero length !");
+	}
+
+	Array buffer(length - 1);
+
+	for (int i = 0, j = 0; i < length; ++i)
+	{
+		if (i == index)
+		{
+			continue;
+		}
+
+		buffer[j] = this->getItem(i);
+		++j;
+	}
+
+	*this = buffer;
+
+	system("cls");
+}
+
 int Array::counter(int pattern, Comparer comparer) const
 {
 	int counter = 0;
@@ -184,12 +395,10 @@ int Array::counter(int pattern, Comparer comparer) const
 	}
 	return counter;
 }
-int* Array::selection(int& newLength, Filter filter) const
+void Array::selection(Array& destination, Filter filter) const
 {
-	newLength = 0;
-
-	int length = getLength();
-	int* buffer = new int[length];
+	int length = getLength(), newLength = 0;
+	Array buffer(length);
 
 	for (int i = 0; i < length; i++)
 	{
@@ -200,43 +409,62 @@ int* Array::selection(int& newLength, Filter filter) const
 		}
 	}
 
-	int* newArray = new int[newLength];
+	Array buffer2(newLength);
 
-	for (int i = 0; i < newLength; i++)
+	for (int i = 0; i < newLength; ++i)
 	{
-		newArray[i] = buffer[i];
+		buffer2[i] = buffer[i];
 	}
 
-	delete[]buffer;
-
-	return newArray;
+	destination = buffer2;
 }
-int* Array::search(int& indexArrayLength, Filter filter)
+void Array::search(Array& indexArray, Filter filter)
 {
-	indexArrayLength = 0;
-	int length = getLength();
-	int* buffer = new int[length];
+	int length = getLength(), newLength = 0;
+	Array buffer(length);
 
-	for (int i = 0, j = 0; i < length; i++)
+	for (int i = 0; i < length; i++)
 	{
 		if (filter(array[i]))
 		{
-			++indexArrayLength;
-			buffer[j] = i;
-			++j;
+			buffer[newLength] = i;
+			++newLength;
 		}
 	}
 
-	int* indexArray = new int[indexArrayLength];
+	Array buffer2(newLength);
 
-	for (int i = 0; i < indexArrayLength; i++)
+	for (int i = 0; i < newLength; ++i)
 	{
-		indexArray[i] = buffer[i];
+		buffer2[i] = buffer[i];
 	}
 
-	delete[]buffer;
+	indexArray = buffer2;
+}
+void Array::screening(Filter filter)
+{
+	int sourceLength = this->getLength(), newLength = 0;
+	Array buffer(sourceLength);
 
-	return indexArray;
+	for (int i = 0, j = 0; i < sourceLength; ++i)
+	{
+		if (!filter(this->getItem(i)))
+		{
+			buffer[newLength] = this->getItem(i);
+			++newLength;
+		}
+	}
+
+	Array buffer2(newLength);
+
+	for (int i = 0; i < newLength; i++)
+	{
+		buffer2[i] = buffer[i];
+	}
+
+	*this = buffer2;
+
+	system("cls");
 }
 
 
@@ -287,18 +515,100 @@ bool Array::operator !=(Array& rho)
 {
 	return !(*this == rho);
 }
-Array& Array:: operator=( Array& rho)
+
+Array& Array:: operator=(Array& rho)
 {
 	int newLength = rho.getLength();
 
 	this->setLength(newLength);
 
+	if (this->array != nullptr)
+	{
+		delete[] this->array;
+	}
+
+	this->array = new int[newLength];
+
 	for (int i = 0; i < newLength; i++)
 	{
-		this->setItem(i,rho.getItem(i));
+		this->setItem(i, rho.getItem(i));
 	}
 
 	return *this;
 }
 
+
+
+
+//void readTxtFile(const char* path, Array& destination)
+//{
+//	ifstream in(path);
+//	int sizeObj = sizeof(Array);
+//	int sizeMaxElement = 100;
+//
+//	if (!in.is_open())
+//	{
+//		cout << "Cannot open file to read!" << endl;
+//		system("pause");
+//		exit(1);
+//	}
+//
+//	char* buffer = new char[sizeMaxElement] {0};
+//	int buff = 0;
+//	for (int i = 0; i < sizeMaxElement; i++)
+//	{
+//		buffer[i] = 0;
+//	}
+//	while (!in.eof())
+//	{
+//
+//	}
+//	//in.destination, sizeObj);
+//
+//	in.close();
+//}//------
+//void stringToArray(char* source, Array& destination)
+//{
+//	int sourceLength = strlen(source), newLength = 0, bufferNumber = 0;
+//	char* buffer1 = new char[sourceLength];
+//	char* buffer2 = new char[sourceLength];
+//	char* space = strchr(source, ' ');
+//	Array bufferArray(sourceLength);
+//
+//	strcpy(buffer2, source);
+//
+//	for (int i = 0; space; ++i, ++newLength)
+//	{
+//		space = strchr(buffer2, ' ');
+//
+//		if (!space)
+//		{
+//			strcpy(buffer1, buffer2);
+//		}
+//		else
+//		{
+//			strncpy(buffer1, buffer2, space - buffer2);
+//			strcpy(buffer2, space + 1);
+//		}
+//
+//		bufferNumber = atoi(buffer1);
+//		bufferArray[i] = bufferNumber;
+//
+//		delete[]buffer1;
+//		buffer1 = new char[sourceLength];
+//	}
+//
+//	Array bufferArray2(newLength);
+//
+//	for (int i = 0; i < newLength; ++i)
+//	{
+//		bufferArray2[i] = bufferArray[i];
+//	}
+//
+//	destination = bufferArray2;
+//
+//	delete[]buffer1;
+//	//delete[]buffer2;
+//	delete space;
+//}
 
--- semester-2/HomeWork11.03/Array.h
+++ semester-2/HomeWork11.03/Array.h
@@ -1,4 +1,8 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
 #include <iostream>
+#include <fstream>
 #include "ctime"
 
 typedef int(*Comparer)(int, int);
@@ -22,18 +26,25 @@ public:
 	void display()const;
 	void sort();
 	void sort(Comparer);
+	void mergeSort(Array&, Comparer);
+	void quickSort(Array&,int,int,Comparer);
 	void inverted();
 	void randomFilling(int);
 	void shuffle();
 	void shift(int,bool);
+	void writeBinFile(const char*);
+	void readBinFile(const char*);
+	void deleteRepits();
+	void deleteElement(int);
 	int counter(int, Comparer)const;
-	int* selection(int&, Filter)const;
-	int* search(int&, Filter);
+	void selection(Array&, Filter)const;
+	void search(Array&, Filter);
+	void screening(Filter);
 
 
 	int& operator [](int);
 	const int& operator [](int)const;
-	Array& operator =(Array&);//нужна помощь
+	Array& operator =(Array&);
 	bool operator ==(Array&);
 	bool operator !=(Array&);
 
@@ -43,3 +54,6 @@ private:
 	static void swap(int&, int&);
 	inline void setLength(int);
 };
+
+
+#endif  ARRAY_H// !ARRAY_H
