6cc9d6d2e6df943533fc4fa276f8f0b0ccb03371
Fifty_Fifty <pashka547@yande.by>
2020-04-08 13:15:26 +0300

    HW01.04

---
+++ semester-2/HomeWork01.04/CW01.04.cpp
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include "WordNumber.h"
+
+using namespace std;
+//В тектстовом файле построчно хранится информация в виде "слово целое число". Нужно на основании файла получить бинарный файл
+//из записей, где каждая запись представляется объектом класса из двух полей:строка и целое число
+
+void transfer(const char*, const char*);
+void read(const char*);
+WordNumber* parse(char*);
+
+int main()
+{
+	transfer("destination.txt", "source.txt");
+	read("destination.txt");
+}
+
+void transfer(const char* destinationPath, const char* sourcePath)
+{
+	ofstream out(destinationPath, ios::binary);
+	ifstream in(sourcePath);
+	char* buffer = new char[WordNumber::N]{ 0 };
+	int bufferNumber = 0, sizeBuf = sizeof(WordNumber);
+	WordNumber* bufferObj;
+
+	while (!in.eof())
+	{
+		in.getline(buffer, sizeBuf);
+		bufferObj = parse(buffer);
+		out.write((char*)bufferObj, sizeBuf);
+		delete bufferObj;
+	}
+
+	in.close();
+	out.close();
+	delete[]buffer;
+}
+
+void read(const char* source)
+{
+	ifstream in(source, ios::binary);
+	int sizeBuf = sizeof(WordNumber);
+	WordNumber* bufferObj = new WordNumber();
+
+	cout << in.tellg();
+
+	while (in.read((char*)bufferObj, sizeBuf))
+	{
+		cout << bufferObj->getWord() << '\t' << bufferObj->getNumber() << endl;
+	}
+
+	in.close();
+}
+
+WordNumber* parse(char* source)
+{
+	char* word = new char[strlen(source)]{ 0 };
+	char* numberChar = new char[strlen(source)]{ 0 };
+	int  numberInt = 0;
+	char* p = strchr(source, ' ');
+
+	strncpy(word, source, (p - source));
+
+	strcpy(numberChar, p + 1);
+	numberInt = atoi(numberChar);
+
+	WordNumber* obj = new WordNumber();
+
+	obj->setWord(word);
+	obj->setNumber(numberInt);
+	
+	return obj;
+}
